fba7164baac567dd30d20a1546d8b011fefbe96a
Christos Uster <christos.uster@gmail.com>
2024-03-14 16:16:31 +0600

    Caching changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import HomeCourses from "@/components/HomeCourses";
 import HomeHeader from "@/components/HomeHeader";
+import { unstable_noStore as noStore } from "next/cache";
 
 export default function Home() {
+  noStore();
   return (
     <div>
       <HomeHeader />
